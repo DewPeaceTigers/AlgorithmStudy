1464a65e51464e44a5b328015bd3a853f8d15357
김예리 <37644146+kinnyeri@users.noreply.github.com>
2023-01-29 20:59:10 +0900

    Update yeri.py

--- weeks/week_40/PG_17677/yeri.py
+++ weeks/week_40/PG_17677/yeri.py
@@ -19,3 +19,36 @@ def solution(str1,str2):
     intersect = len(set1|set2)
     if intersect == 0 : return 65536
     return int(comb/intersect*65536)
+
+
+
+##############
+
+def div(str):
+    res = []
+    temp = str[0].lower()
+    for i in range(1,len(str)):
+        word = str[i].lower()
+        if (temp+word).isalpha():
+            res.append(temp+word)
+        temp = word
+    return res
+def erase(str_list):
+    res = []
+    for str in str_list:
+        if str.isalpha():
+            res.append(str)
+    return res
+def solution(str1, str2):
+    answer = 0
+    list_1 = div(str1)
+    list_2 = div(str2)
+    same = []
+    whole = []
+    for word in list_1:
+        if word in list_2:
+            same.append(word)
+            list_2.remove(word)
+        whole.append(word)
+    if list_2 : whole.extend(list_2)
+    return 65536 if len(same)==0 and len(whole) == 0 else int(len(same)/len(whole)*65536)
